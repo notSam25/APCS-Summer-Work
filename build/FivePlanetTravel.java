5683c3bb84cbf4a142c0df458b45a9d6c2162efb
Samuel Wiseman <samw@gov.com>
2022-06-25 13:52:47 -0500

    add Summer Work

---
+++ build/FivePlanetTravel.java
@@ -0,0 +1,34 @@
+package build;
+public class FivePlanetTravel {
+  /*
+   * NumPlanets = total planets to traverse
+   * Velocity = travel velocity(mp/h)
+   */
+  public static int NumPlanets = 5, Velocity = 670616629;
+
+  static double CalculateTime(final int planetDistance) {
+    return (double) planetDistance / Velocity;
+  } 
+
+  public static void main(String[] args)
+  {
+    // planet distances from: https://theplanets.org/
+    final int[] planetArray = {
+      56974146, // Mercury
+      25724767, // Venus
+      48678219, // Mars
+      390674710,// Jupiter
+      792248270 // Saturn
+    };
+
+    // total travel time(Hours)
+    double totalTravelTime = 0;
+
+    /* your code here */
+    for(int i = 0; i < NumPlanets; i++) {
+      totalTravelTime += CalculateTime(planetArray[i]);
+      System.out.printf("Calculating time for planetArray[%d]\n", i);
+    }
+    System.out.printf("Total travel time(hours) -> %f\n", totalTravelTime);
+  }
+}
