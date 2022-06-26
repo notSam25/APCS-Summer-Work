04a8d74e121e8a7526367f7a40793e379e359936
Samuel Wiseman <samw@gov.com>
2022-06-26 14:59:14 -0500

    problem 3.4.x complepted

--- src/PoThePanda/3.4/CavemenEscape.java
+++ src/PoThePanda/3.4/CavemenEscape.java
@@ -1,17 +1,25 @@
 public class CavemenEscape {
     public static void main(String[] args){
-        /*
+        /* Question Problem
         Due to his arrogance and utter stupidity, Po ignored your advice in the 
         previous activity and has now been captured by the cavemen. They will only 
         let Po escape if he can correctly spell out the name of an integer. For example, 
         if the cavemen choose the number 3, Po must spell out “three” to survive. Based on 
         the integer that the cavemen choose, print out exactly what Po should spell in order to survive. 
         */
-        int cavemenChoice = (int)(5*(Math.random())+1); //Randomly generates the number the cavemen want Po to spell out(A random number between 1 and 5)
-        System.out.println("Cavemen chose the number "+ cavemenChoice);//Prints out the cavemens' choice so that you can check if your code worked as intended
 
-        //Below, type a series of else if statement that will print out the spelling of the number the cavemne chose.
-        //HINT: You can have multiple subsequent else if statements before you close with an else statemnet
-        
+        int cavemenChoice = (int)(5*(Math.random())+1); // Randomly number 1-5
+        String Number = "Invalid Number";
+
+        switch(cavemenChoice) {
+            case 1: Number = "one";   break;
+            case 2: Number = "two";   break;
+            case 3: Number = "three"; break;
+            case 4: Number = "four";  break;
+            case 5: Number = "five";  break;
+            default: break;
+        }
+
+        System.out.printf("Number to spell(integer, string) -> [ %d, %s ]\n", cavemenChoice, Number);
     }
 }
