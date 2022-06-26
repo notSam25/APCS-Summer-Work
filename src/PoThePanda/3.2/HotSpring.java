e21f842f3c0b3078d2e8f5e6687aec7f305b74d0
Samuel Wiseman <samw@gov.com>
2022-06-26 14:27:22 -0500

    problem 3.2.x completed

--- src/PoThePanda/3.2/HotSpring.java
+++ src/PoThePanda/3.2/HotSpring.java
@@ -1,16 +1,14 @@
 public class HotSpring {
     public static void main(String[] args){
-        /*
+        /* Question Problem
         Po wants to relax in his local hot spring, which has a labeled 
         thermometer on the outside. Given that Po will die instantly if 
         he experiences any temperature over 100 degrees Fahrenheit, please 
         print out a warning statement if his life is in imminent danger.
         */
-        int springTemp = (int)(130*(Math.random())+1); //Randomly generates the hot spring's temperature (A random number between 1 and 130)
-        System.out.println("Actual spring temperature is "+ springTemp);//Prints out the hot spring's temperature so that you can check if your code worked as intended
 
-        //Below, type an if statement that will print out "Hot spring is safe for Po" if the hot spring's temperature is less than or equal to Po's maximum temperature, 100
-
-
+        int springTemp = (int)(130*(Math.random())+1); // Random number 1-130
+        
+        System.out.printf("Will Po die(temperature, death) -> [ %d, %B ]\n", springTemp, springTemp > 100);
     }
 }
--- src/PoThePanda/3.2/TreeClimb.java
+++ src/PoThePanda/3.2/TreeClimb.java
@@ -6,12 +6,8 @@ public class TreeClimb {
         be weak. Given Po’s weight and the maximum load of the tree branch, 
         print out a warning statement if Po’s weight exceeds the maximum load of the tree branch
         */
-        int poWeight = 30; //Po weighs 100 pounds
-        int maxTreeLoad = (int)(50*(Math.random())+1); //Randomly generates the tree's maximum load (A random number between 1 and 50)
-        System.out.println("Actual tree load is "+ maxTreeLoad);//Prints out the tree's maximum load so that you can check if your code worked as intended
-
-        //Below, type an if statement that will print out "Tree is safe for Po" if Po's weight(30) is less than or equal to the tree's maximum load
-
-
+        int poWeight = 30; //Po weight
+        int maxTreeLoad = (int)(50*(Math.random())+1); // Random number 1-50
+        System.out.printf("Tree climbable(max load, climbable) -> [ %d, %B ]\n", maxTreeLoad, maxTreeLoad <= poWeight);
     }
 }
