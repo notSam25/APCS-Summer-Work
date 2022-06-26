8bb9f51f8ae44a05f7d93252a18c52ff2b8de5c2
Samuel Wiseman <samw@gov.com>
2022-06-26 14:34:40 -0500

    problems 3.3.x completed

--- src/PoThePanda/3.3/KungFuPanda.java
+++ src/PoThePanda/3.3/KungFuPanda.java
@@ -1,16 +1,15 @@
 public class KungFuPanda {
     public static void main(String[] args){
-        /*
+        /* Question Problem
         After realizing that he has no hobbies, Po decides to start 
         learning kung fu. However, the only kung fu class nearby only 
         accepts students 12 and above. Given Po’s age, tell Po whether he 
         can or cannot attend kung fu class using if/else statements.
         */
-        int poAge = (int)(15*(Math.random())+1); //Randomly generates Po's age(A random number between 1 and 15)
-        System.out.println("Po's age is "+ poAge);//Prints out the Po's age so that you can check if your code worked as intended
 
-        //Below, type an if else statement that will print out "Po can attend Kung Fu class" if Po is 12 or older, and print "Po can't attend Kung Fu Class" if not
-
+        final int minAcceptedAge = 12;
+        int poAge = (int)(15*(Math.random())+1); // Random 1-15
         
+        System.out.printf("Can attend class(age, can attend) -> [ %d, %B ]\n", poAge, poAge >= minAcceptedAge);
     }
 }
